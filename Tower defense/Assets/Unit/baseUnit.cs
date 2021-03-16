3f6a9ba30313c34f50a0f937697ec49a0f8caf44
ashklyarsky@sinergo.ry <tw1nkyy@yandex.ru>
2021-03-16 19:49:04 +0700

    BaseUnit

---
+++ Tower defense/Assets/Unit/baseUnit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class baseUnit : MonoBehaviour
+{
+
+    public float speed;
+    public int hp;
+    public int damage;
+
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MovementLogic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * speed);
+    }
+}
